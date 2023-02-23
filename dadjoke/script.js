62ebee25cb1248eb3d432f1aa4a96bf3cddebfb5
Aamir Razdan <65817609+Aamir-Razdan@users.noreply.github.com>
2023-02-23 13:23:35 +0530

    Add files via upload

---
+++ dadjoke/script.js
@@ -0,0 +1,37 @@
+const joke=document.getElementById('joke');
+const jokebtn=document.getElementById('jokeButton');
+
+jokebtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  joke.innerHTML = data.joke
+}
+
+
+//USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       joke.innerHTML = data.joke
+//     })
+// }
